125a7786c13a22c6273081c12650a73e026460b0
Katakung95 <tanwasripan@hotmail.co.uk>
2022-05-19 22:06:51 +0100

    redo on local machine - first test passed

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// use bodyParser middleware
+app.use("/api/users",bodyParser.urlencoded({extended: false}));
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,6 +17,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// create Schema for user
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true}
+});
+
+const User = mongoose.model("User", userSchema);
+
+// test 2 
+app.post("/api/users", (req, res) => {
+  let username = req.body["username"];
+  User.create({username: username}, function (err, user){
+    if (err) console.error(err);
+    res.json({username: username, _id: user.id});
+  })
+});
 
 
 
