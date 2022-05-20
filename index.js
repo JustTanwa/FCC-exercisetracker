8f002f2652369dd3ec63df740ec9eb092f1d3410
Katakung95 <tanwasripan@hotmail.co.uk>
2022-05-20 17:49:07 +0100

    test 3 for getting users as an array of objects

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 // use bodyParser middleware
-app.use("/api/users",bodyParser.urlencoded({extended: false}));
+app.use('/api/users', bodyParser.urlencoded({ extended: false }));
 
-
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 // create Schema for user
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true}
+	username: { type: String, required: true },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
-// test 2 
-app.post("/api/users", (req, res) => {
-  let username = req.body["username"];
-  User.create({username: username}, function (err, user){
-    if (err) console.error(err);
-    res.json({username: username, _id: user.id});
-  })
+// test 2
+app.post('/api/users', (req, res) => {
+	let username = req.body['username'];
+	User.create({ username: username }, function (err, user) {
+		if (err) console.error(err);
+		res.json({ username: username, _id: user.id });
+	});
 });
 
+// test 3 get users
 
-
+app.get('/api/users', (req, res) => {
+	User.find(function (err, users) {
+		if (err) console.error(err);
+		res.send(users);
+	});
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+	console.log('Your app is listening on port ' + listener.address().port);
+});
